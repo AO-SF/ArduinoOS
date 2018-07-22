eedf83c663a43ddad9d27f730ffc3f78ad8fc9c7
DJW <dan@fake.com>
2018-07-22 22:30:20 +0100

    Added 'instruction' concept to the assembler allowing us to parse db and dw instructions. Also improved output

--- bin/helloworld.s
+++ bin/helloworld.s
@@ -1,14 +1,14 @@
 db message 'Hello world!\n'
 dw messageLen 13
 
-; Print 'Hello world!'
-set r0 1 ; write syscall
-set r1 0 ; stdout
-set r2 message
-set r3 messageLen
+; Print message from program memory
+mov r0 1 ; write syscall (version which uses program memory)
+mov r1 0 ; stdout fd
+mov r2 message
+mov r3 messageLen
 syscall
 
 ; Exit
-set r0 2
-set r1 0
+mov r0 2
+mov r1 0
 syscall
