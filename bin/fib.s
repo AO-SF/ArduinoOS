c6e63a5ff671eec5ed0839fae40d1b6358380073
DJW <dan@fake.com>
2018-07-23 01:35:39 +0100

    Updated fib.s to use new skiplt instruction, saving 7 instructions

--- bin/fib.s
+++ bin/fib.s
@@ -11,14 +11,8 @@ syscall
 ; Time to end?
 mov r3 40000
 cmp r3 r5 r3
-mov r2 4 ; 4 is shift for less than
-shr r3 r3 r2
-mov r2 1
-and r3 r3 r2 ; r3 now contains 0/1 as result of (r5<40000)
-mov r2 3 ; 'jmp loopend' needs 3 bytes (set16 r7 loopendaddr)
-mul r3 r3 r2
-add r7 r7 r3 ; if r5<40000 is false this is a nop and we break out of the loop on the next instruction,
-jmp loopend ; otherwise this jump is skipped and we continue executing the loop
+skiplt r3 ; if r5<40000 is true this causes us to skip the next instruction and continue executing the loop
+jmp loopend ; but if false then we end up here and break out of the loop
 
 ; Perform single Fibonacci step
 add r4 r5 r6
