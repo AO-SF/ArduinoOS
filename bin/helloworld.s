534c5c47b8b43625c68fbc0d6e3c94b19ae1e67c
DJW <dan@fake.com>
2018-07-23 13:08:32 +0100

    Formalised syscalls and added a couple more

--- bin/helloworld.s
+++ bin/helloworld.s
@@ -2,13 +2,13 @@ db message 'Hello world!\n'
 dw messageLen 13
 
 ; Print message from program memory
-mov r0 1 ; write syscall (version which uses program memory)
-mov r1 0 ; stdout fd
+mov r0 258 ; write syscall (version which uses program memory)
+mov r1 1 ; stdout fd
 mov r2 message
 mov r3 messageLen
 syscall
 
 ; Exit
-mov r0 2
+mov r0 0
 mov r1 0
 syscall
