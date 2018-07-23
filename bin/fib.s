9f8260c8d862c5d47aa9847e00a9e365ecbc9afc
DJW <danielwhite94@yahoo.co.uk>
2018-07-24 00:28:04 +0100

    Improved output of fib.s and used printChar to simplify

--- bin/fib.s
+++ bin/fib.s
@@ -2,8 +2,6 @@ jmp start
 
 include lib.s
 
-db newline '\n'
-
 label start
 ; Init sequence
 mov r4 0 ; r4 is the lower of the two current values
@@ -17,12 +15,6 @@ mov r0 r4
 call printDec
 pop r5
 pop r4
-; Print newline
-mov r0 258 ; write syscall (from progmem)
-mov r1 1 ; stdout fd
-mov r2 newline
-mov r3 1
-syscall
 
 ; Time to end?
 mov r3 40000
@@ -30,13 +22,27 @@ cmp r3 r4 r3
 skiplt r3 ; if r4<40000 is true this causes us to skip the next instruction and continue executing the loop
 jmp loopend ; but if false then we end up here and break out of the loop
 
+; Print comma
+push r4
+push r5
+mov r0 ','
+call printChar
+mov r0 32 ; TODO: Use ' ' constant once supported
+call printChar
+pop r5
+pop r4
+
 ; Perform single Fibonacci step
 add r3 r4 r5 ; add two numbers and store into temp register
 mov r4 r5 ; move higher into lower
 mov r5 r3 ; move sum into higher
 
 jmp loopstart
+
+; Print newline to terminate list
 label loopend
+mov r0 '\n'
+call printChar
 
 ; Exit
 mov r0 0
