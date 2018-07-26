be893f939bb448de5cf047c6bf4ae589711a1806
DJW <danielwhite94@yahoo.co.uk>
2018-07-26 13:47:17 +0100

    Moved kernel debugging code out to new module

---
+++ kernel/debug.c
@@ -0,0 +1,89 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "debug.h"
+#include "kernelfs.h"
+
+void debugFsHelper(const char *path, int indent);
+
+void debugFileIo(void) {
+	#define DATALEN 16
+	uint8_t data[DATALEN];
+
+	// Open serial and random number generator
+	KernelFsFd serialFd=kernelFsFileOpen("/dev/ttyS0");
+	KernelFsFd randFd=kernelFsFileOpen("/dev/urandom");
+
+	// Write bytes from random generator out to serial/stdout
+	KernelFsFileOffset readCount=kernelFsFileRead(randFd, data, DATALEN);
+	for(int i=0; i<readCount; ++i) {
+		char str[32];
+		sprintf(str, "%i\n", data[i]);
+		kernelFsFileWrite(serialFd, (uint8_t *)str, strlen(str)+1);
+	}
+
+	// Close files
+	kernelFsFileClose(randFd);
+	kernelFsFileClose(serialFd);
+}
+
+void debugFs(void) {
+	debugFsHelper("/", 0);
+}
+
+void debugFsHelper(const char *path, int indent) {
+	// handle indent
+	for(int i=0; i<indent; ++i)
+		printf("  ");
+
+	// open file/dir
+	KernelFsFd fd=kernelFsFileOpen(path);
+	if (fd==KernelFsFdInvalid) {
+		printf("error openning file '%s'\n", path);
+		return;
+	}
+
+	// print name
+	char pathCopy[KernelFsPathMax];
+	strcpy(pathCopy, path);
+	char *dirname, *basename;
+	if (strcmp(path, "/")==0)
+		dirname="", basename="/";
+	else
+		kernelFsPathSplit(pathCopy, &dirname, &basename);
+
+	printf("%s", basename);
+
+	// print first few bytes of content
+#define CONTENTSIZE 8
+	if (strcmp(path, "/dev/ttyS0")!=0) {
+		for(int i=0; i<16-strlen(basename); ++i)
+			printf(" ");
+		uint8_t content[CONTENTSIZE];
+		KernelFsFileOffset readCount=kernelFsFileRead(fd, content, CONTENTSIZE);
+		printf(" data:");
+		for(KernelFsFileOffset i=0; i<readCount; ++i)
+			printf(" 0x%02X", content[i]);
+		if (readCount==CONTENTSIZE)
+			printf(" ...");
+	}
+#undef CONTENTSIZE
+
+	printf("\n");
+
+	// If directory print children recursively
+	if (1) { // TODO: check if is directory
+		char childPath[KernelFsPathMax];
+		unsigned childNum;
+		for(childNum=0; ; ++childNum) {
+			if (!kernelFsDirectoryGetChild(fd, childNum, childPath))
+				break;
+			debugFsHelper(childPath, indent+1);
+		}
+	}
+
+	// close file
+	kernelFsFileClose(fd);
+}
---
+++ kernel/debug.h
@@ -0,0 +1,7 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+void debugFileIo(void);
+void debugFs(void);
+
+#endif
