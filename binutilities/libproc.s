c8beefc51d5445e67a28db4a3bb2937bf3c02a99
DJW <dan@fake.com>
2018-07-27 18:20:16 +0100

    Added getabspath to libproc.s

--- binutilities/libproc.s
+++ binutilities/libproc.s
@@ -1,8 +1,47 @@
+require libstr.s
+
+db libprocSlashStr '/', 0
+
 ; getpwd(r0=dest addr)
 label getpwd
 mov r1 r0
 mov r0 514
 syscall
+ret
+
+; getabspath(r0=dest addr, r1=src path addr) - checks if path is absolute, and if not makes it so, stores result in dest addr either way
+label getabspath
+
+; Set r0 to empty string
+mov r2 0
+store8 r0 r2
+
+; Test first char for '/'
+load8 r2 r1
+mov r3 '/'
+cmp r2 r2 r3
+skipneq r2
+jmp getabspathnext ; already has a '/', continue
+
+; Add pwd
+push r0
+push r1
+call getpwd
+pop r1
+pop r0
+
+; Add '/'
+push r0
+push r1
+mov r1 libprocSlashStr
+call strcat
+pop r1
+pop r0
+
+label getabspathnext
+; Add src string to dest (r0 and r1 already setup)
+call strcat
+
 ret
 
 ; exit (r0=status) - does not return
