ed1e5140b02283943a9b1936e45fff9b0a56367e
DJW <dan@fake.com>
2018-07-27 16:19:39 +0100

    Added strlen and strtrimlast

--- binutilities/libstr.s
+++ binutilities/libstr.s
@@ -26,3 +26,80 @@ jmp strcpyLoopStart
 
 label strcpyDone
 ret
+
+; r0=strchr(r0=haystack addr, r1=needle char), r0 is addr of first occurence, or 0 on failure
+label strchr
+
+mov r2 0 ; loop index
+
+label strchrLoopStart
+
+; load character
+mov r3 r0
+add r3 r3 r2
+load8 r3 r3 ; character stored in r3 for now
+
+; hit matching character?
+cmp r4 r3 r1
+skipneq r4
+jmp strchrLoopEnd
+
+; reached null terminator?
+cmp r4 r3 r3
+skipneqz r4
+jmp strchrNotFound
+
+; try next character
+inc r2
+jmp strchrLoopStart
+
+label strchrLoopEnd
+add r0 r0 r2 ; update r0 to point into string
+ret
+
+label strchrNotFound
+mov r0 0
+ret
+
+; r0=strlen(r0=str addr)
+label strlen
+
+mov r2 0 ; loop index
+
+label strlenLoopStart
+
+; load character
+mov r3 r0
+add r3 r3 r2
+load8 r3 r3 ; character stored in r3 for now
+
+; reached null terminator?
+cmp r4 r3 r3
+skipneqz r4
+jmp strlenLoopEnd
+
+; try next character
+inc r2
+jmp strlenLoopStart
+
+label strlenLoopEnd
+mov r0 r2 ; move index into return register
+ret
+
+; strtrimlast(r0=str addr) - trims last character
+label strtrimlast
+
+; Find addr of last char
+push r0
+call strlen
+mov r1 r0
+pop r0
+
+add r2 r0 r1
+dec r2
+
+; Insert null byte to truncate
+mov r3 0
+store8 r2 r3
+
+ret
