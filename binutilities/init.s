5b4a10abc55f62a28a6f258bafccd92472f4f139
DJW <dan@fake.com>
2018-07-27 01:04:58 +0100

    Began writing shell and updated init to run it

--- binutilities/init.s
+++ binutilities/init.s
@@ -1,40 +1,13 @@
-; Example init file - calls fork & exec to run another program
+; Simply execs shell
 
-db execPath '/bin/helloworld2.o', 0
+db execPath '/bin/sh.o', 0
 
-; Call fork
-mov r0 4
-syscall
-
-; Check fork return
-mov r1 64 ; ProcManPidMax
-cmp r1 r0 r1
-skipneqz r1
-jmp forkRetChild
-skipneq r1
-jmp forkRetFail
-
-label forkRetParent
-jmp success;
-
-label forkRetChild
-; Exec
+; call exec
 mov r0 5
 mov r1 execPath
 syscall
-jmp error ; exec only returns on failure
 
-label forkRetFail
-jmp error;
-
-; Exit (success)
-label success
-mov r0 0
-mov r1 0
-syscall
-
-; Exit (failure)
-label error
+; exec only returns on failure
 mov r0 0
 mov r1 1
 syscall
