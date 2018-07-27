75436c27a8bd9eb23d735643e32bf57155e380ef
DJW <danielwhite94@yahoo.co.uk>
2018-07-27 18:19:48 +0100

    Added strcat to libstr.s

--- binutilities/libstr.s
+++ binutilities/libstr.s
@@ -103,3 +103,20 @@ mov r3 0
 store8 r2 r3
 
 ret
+
+; strcat(r0=dest, r1=src)
+label strcat
+
+; Find length of dest
+push r0
+push r1
+call strlen
+mov r2 r0 ; length in r2
+pop r1
+pop r0
+
+; Copy src to end of dest
+add r0 r0 r2 ; r0 starts with dest, ends with dest+len
+call strcpy ; r1 already has src
+
+ret
