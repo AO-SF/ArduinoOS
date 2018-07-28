01496e2ad78e1b1c6ddd0705166d4846e8d6de23
DJW <dan@fake.com>
2018-07-28 22:18:10 +0100

    builder now makes all volumes maximum 8kb size

--- builder/builder.c
+++ builder/builder.c
@@ -15,11 +15,11 @@ uint8_t readFunctor(uint16_t addr, void *userData);
 void writeFunctor(uint16_t addr, uint8_t value, void *userData);
 
 int main(int agrc, char **argv) {
-	buildVolume("bin", 7*1024, "../binmockup");
-	buildVolume("libstdio", 7*1024, "../binutilities/lib/std/io");
-	buildVolume("libstdmath", 7*1024, "../binutilities/lib/std/math");
-	buildVolume("libstdproc", 7*1024, "../binutilities/lib/std/proc");
-	buildVolume("libstdstr", 7*1024, "../binutilities/lib/std/str");
+	buildVolume("bin", 8*1024, "../binmockup");
+	buildVolume("libstdio", 8*1024, "../binutilities/lib/std/io");
+	buildVolume("libstdmath", 8*1024, "../binutilities/lib/std/math");
+	buildVolume("libstdproc", 8*1024, "../binutilities/lib/std/proc");
+	buildVolume("libstdstr", 8*1024, "../binutilities/lib/std/str");
 
 	return 0;
 }
