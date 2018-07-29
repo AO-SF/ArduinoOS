131f0f5a96eb85f8fc25a0ef5652cc3d3b9b2587
DJW <dan@fake.com>
2018-07-29 20:53:55 +0100

    Added tty utility

---
+++ binutilities/tty.s
@@ -0,0 +1,37 @@
+jmp start
+
+require lib/std/io/fput.s
+require lib/std/proc/exit.s
+
+ab pathBuf 64
+
+label start
+
+; Grab stdio fd
+mov r0 512
+syscall
+
+; Grab path
+mov r1 r0
+mov r0 261
+mov r2 pathBuf
+syscall
+
+; Check return
+; cmp r0 r0 r0
+; skipeqz r0
+; jmp print
+;
+; mov r0 1
+; call exit
+
+; Print path
+;label print
+mov r0 pathBuf
+call puts0
+mov r0 '\n'
+call putc0
+
+; Exit
+mov r0 0
+call exit
