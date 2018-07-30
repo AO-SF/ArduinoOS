a34fd9920f137a5addef39239cbbf84e1083a49b
DJW <dan@fake.com>
2018-07-30 21:18:19 +0100

    Added int32add32

--- binutilities/lib/std/math/int32.s
+++ binutilities/lib/std/math/int32.s
@@ -63,3 +63,19 @@ add r2 r2 r1
 store16 r0 r2
 
 ret
+
+; int32add32(dest=r0, opA=r1)
+label int32add32
+
+; Add upper part (not worrying about overflow)
+load16 r2 r0
+load16 r3 r1
+add r2 r2 r3
+store16 r0 r2
+
+; Add lower part (which may overflow into upper part)
+inc2 r1
+load16 r1 r1
+call int32add16
+
+ret
