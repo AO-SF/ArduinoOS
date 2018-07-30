5cb11077f89119d6d842ed40ee0e5c004971a026
DJW <dan@fake.com>
2018-07-30 21:17:40 +0100

    Added int32setUpper16 and int32set16shift

--- binutilities/lib/std/math/int32.s
+++ binutilities/lib/std/math/int32.s
@@ -8,6 +8,25 @@ inc2 r0
 store16 r0 r1
 ret
 
+; int32setUpper16(dest=r0, src=r1) ; 32 bit dest ptr = (16 bit src value)<<16
+label int32setUpper16
+store16 r0 r1
+inc2 r0
+mov r2 0
+store16 r0 r2
+ret
+
+; int32set16shift(dest=r0, src=r1, shift=r2) ; like int32set16 but also shifts src first
+label int32set16shift
+mov r3 16
+sub r3 r3 r2
+shr r3 r1 r3
+store16 r0 r3
+inc2 r0
+shl r3 r1 r2
+store16 r0 r3
+ret
+
 ; int32add16(dest=r0, src=r1)
 label int32add16
 ; Compute maximum we can add to dest.lower before overflowing
