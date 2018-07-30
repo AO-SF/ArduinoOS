bae9d55169f1f75aa544bb3dd8dc5c6873023b37
DJW <dan@fake.com>
2018-07-30 03:56:56 +0100

    Added kill utility

---
+++ binutilities/kill.s
@@ -0,0 +1,33 @@
+jmp start
+
+require lib/std/proc/exit.s
+require lib/std/str/strtoint.s
+
+ab argBuf 64
+
+label start
+
+; Grab pid from first argument
+mov r0 3
+mov r1 1
+mov r2 argBuf
+mov r3 64
+syscall
+
+cmp r1 r0 r0
+skipneqz r1
+jmp done
+
+; Convert to integer
+mov r0 argBuf
+call strtoint
+
+; Kill
+mov r1 r0
+mov r0 10
+syscall
+
+; Exit
+label done
+mov r0 0
+call exit
