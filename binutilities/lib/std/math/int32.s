96394dd2e5d983403b465befbfa33d8d4265d9eb
DJW <dan@fake.com>
2018-07-30 22:28:03 +0100

    added int32div16

--- binutilities/lib/std/math/int32.s
+++ binutilities/lib/std/math/int32.s
@@ -193,3 +193,43 @@ pop r1
 pop r2
 
 ret
+
+; int32div16(r0=dest, r1=divisor) - divide 32 bit quantity at r0 by 16 bit divisor in r1
+label int32div16
+
+; TODO: Use a better (constant time) algorithm
+mov r2 0 ; result counter
+label int32div16loopstart
+; Is the value smaller than the divisor?
+load16 r3 r0
+cmp r3 r3 r3
+skipeqz r3
+jmp int32div16loopnext
+
+inc2 r0
+load16 r3 r0
+dec2 r0
+cmp r3 r3 r1
+skipge r3
+jmp int32div16loopend
+label int32div16loopnext
+
+; Subtract divisor from dest
+push r0
+push r1
+push r2
+call int32sub16
+pop r2
+pop r1
+pop r0
+
+; Inc result counter and loop again
+inc r2
+jmp int32div16loopstart
+
+; Done
+label int32div16loopend
+mov r1 r2
+call int32set16
+
+ret
