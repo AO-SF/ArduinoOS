9abf7b650c77cb290b35205ec1a4dbb2fa2bf989
DJW <danielwhite94@yahoo.co.uk>
2018-08-06 00:17:34 +0100

    Added cursesClearScreen and cursesSetPosXY

--- binutilities/lib/curses/curses.s
+++ binutilities/lib/curses/curses.s
@@ -1,12 +1,29 @@
-; .....
-;
-;
-;
-;
-;
+requireend ../std/io/fput.s
+requireend ../std/io/fputdec.s
 
-label cursesInit
+db cursesEscSeqStrClear 27, '[2J', 0
+
+label cursesClearScreen
+mov r0 cursesEscSeqStrClear
+call puts0
 ret
 
-label cursesQuit
+; cursesSetPosXY(x=r0, y=r1)
+label cursesSetPosXY
+push r0
+push r1
+
+mov r0 27
+call putc0
+mov r0 '['
+call putc0
+pop r0 ; y
+call putdec
+mov r0 ';'
+call putc0
+pop r0 ; x
+call putdec
+mov r0 'H'
+call putc0
+
 ret
