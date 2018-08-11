baa334159c3e3cda1e647af6c0315bffca6a5e33
DJW <dan@fake.com>
2018-08-11 22:39:32 +0100

    Added tilesGetPrintString

--- src/userspace/rpg/tiles.s
+++ src/userspace/rpg/tiles.s
@@ -4,4 +4,12 @@
 ; with the 15th (final) byte used for flags
 ;
 ; Current tiles are: 0 - water, 1 - grass, 2 - tree.
+const tileDataSize 16
 db tileData 27,'[','3','4',';','1',';','4','4','m',226,150,147,0,0,0,	27,'[','3','2',';','1',';','4','2','m',226,150,145,0,0,0,	27,'[','3','3',';','4','2','m',240,159,140,178,0,0,0,0
+
+label tilesGetPrintString ; takes tile id in r0, returns addr in r0
+mov r1 tileDataSize
+mul r0 r0 r1
+mov r1 tileData
+add r0 r0 r1
+ret
