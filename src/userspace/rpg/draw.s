1a4e08dbc937374843c164317a010945763bc9e3
DJW <danielwhite94@yahoo.co.uk>
2018-08-11 23:00:25 +0100

    Implemented rpg redraw

--- src/userspace/rpg/draw.s
+++ src/userspace/rpg/draw.s
@@ -5,7 +5,44 @@ requireend level.s
 requireend tiles.s
 
 label redraw
-; TODO: clear screen, move to start of each row with curses, call drawCellRaw on each cell in row, move onto next row
+; clear screen and return to top left
+call cursesReset
+; row loop init
+mov r1 0 ; current row
+label redrawRowLoopStart
+; drawn all rows?
+mov r4 levelH
+cmp r4 r1 r4
+skiplt r4
+jmp redrawRowLoopEnd
+; draw first cell of this row (this moves the cursor for us)
+mov r0 0 ; column=0
+push r1
+call updateCell
+pop r1
+; loop over all other cells in this row
+mov r2 1 ; current column
+label redrawColumnLoopStart
+; Reached end of row?
+mov r4 levelW
+cmp r4 r2 r4
+skiplt r4
+jmp redrawColumnLoopEnd
+; Draw this cell
+push r1
+push r2
+mov r0 r2
+call drawCellRaw
+pop r2
+pop r1
+; Advance to next column
+inc r2
+jmp redrawColumnLoopStart
+label redrawColumnLoopEnd
+; Advance to next row
+inc r1
+jmp redrawRowLoopStart
+label redrawRowLoopEnd
 ret
 
 label drawCellRaw ; takes x and y in r0 and r1
