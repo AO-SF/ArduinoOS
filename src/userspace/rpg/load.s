1738c664178af7152612ea1509a91dd319bb63b6
DJW <danielwhite94@yahoo.co.uk>
2018-08-11 22:39:43 +0100

    Fixed bug in rpg levelLoad and speeded it up

--- src/userspace/rpg/load.s
+++ src/userspace/rpg/load.s
@@ -3,8 +3,6 @@ requireend ../lib/std/proc/openpath.s
 
 requireend level.s
 
-ab loadScratchByte 1
-
 label loadLevel ; takes path in r0, returns boolean success value in r0
 ; attempt to open given path
 call openpath
@@ -12,37 +10,21 @@ mov r1 r0
 cmp r5 r1 r1
 skipneqz r5
 jmp loadLevelError
-; load loop init (fd is in r1)
-mov r2 0 ; loop index/file offset
-label loadLevelLoopStart
-; loaded full level size?
-mov r5 levelWH
-cmp r5 r2 r5
-skipneq r5
-jmp loadLevelLoopEnd
-; attempt to read next byte
+; read level
 mov r0 256
-mov r3 loadScratchByte
-mov r4 1
+mov r2 0
+mov r3 levelArray
+mov r4 levelWH
 syscall
-cmp r0 r0 r0
-skipneqz r0
-jmp loadLevelError
-; store byte into level array
-mov r0 levelArray
-add r0 r0 r2
-mov r5 loadScratchByte
-load8 r5 r5
-store8 r0 r5
-; loop around to read next byte
-inc r2
-jmp loadLevelLoopStart
-label loadLevelLoopEnd
+; check read return for later
+mov r4 levelWH
+cmp r4 r0 r4
 ; close file
-mov r1 r0
 mov r0 256
 syscall
-; return success
+; return success/failure
+mov r0 0
+skipneq r4
 mov r0 1
 ret
 ; return failure
