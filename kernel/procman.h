5551df76b280191c633db01d1eb327bc6ade71f8
DJW <danielwhite94@yahoo.co.uk>
2018-08-11 15:56:49 +0100

    Added ProcManExitStatus as new argument to kernel's procManProcessKill. Also now pass exit status back in r0 from a waitpid syscall.

--- kernel/procman.h
+++ kernel/procman.h
@@ -6,6 +6,12 @@
 typedef uint8_t ProcManPid;
 #define ProcManPidMax 64
 
+typedef enum {
+	ProcManExitStatusSuccess=0, // 0 and all other unused values can be used when calling exit
+	ProcManExitStatusKilled=65534, // process was killed by either a syscall or the kernel itself (rather than the process itself calling exit)
+	ProcManExitStatusTimeout=65535, // waitpid timed out (process is still running)
+} ProcManExitStatus;
+
 ////////////////////////////////////////////////////////////////////////////////
 // General functions
 ////////////////////////////////////////////////////////////////////////////////
@@ -22,7 +28,7 @@ int procManGetProcessCount(void);
 ////////////////////////////////////////////////////////////////////////////////
 
 ProcManPid procManProcessNew(const char *programPath); // Returns ProcManPidMax on failure
-void procManProcessKill(ProcManPid pid);
+void procManProcessKill(ProcManPid pid, ProcManExitStatus exitStatus);
 
 void procManProcessTick(ProcManPid pid);
 
