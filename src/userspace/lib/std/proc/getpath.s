97392cff544c8c0545fe5e228a2de881b73131d8
DJW <dan@fake.com>
2018-08-12 19:22:03 +0100

    Fixed bugs in openpath and getpath

--- src/userspace/lib/std/proc/getpath.s
+++ src/userspace/lib/std/proc/getpath.s
@@ -8,23 +8,30 @@ db getpathSlashStr '/', 0
 
 ; getpath(r0=dest path, r1=src path) - interprets path as the shell would - checking in PATH first if path has no slashes, otherwise making absolute if needed by preprending pwd
 label getpath
-; Copy src path into dest
+; Copy src path into dest and getpathScratchBuf
 push r0
+push r1
 call strcpy
+pop r1
+push r1
+mov r0 getpathScratchBuf
+call strcpy
+pop r1
 pop r0
-
 ; Check if the path contains no slashes,
 push r0
+push r1
 mov r1 '/'
 call strchr
 ; If this is true, we try first looking in PATH for the file.
 cmp r2 r0 r0
+pop r1
 pop r0
 skipeqz r2
 jmp getpathTry
-
 ; No slashes found, create combined path to try
 push r0
+push r1
 mov r0 516 ; getpath
 mov r1 getpathScratchBuf
 syscall
@@ -35,26 +42,25 @@ mov r0 getpathScratchBuf
 pop r1
 push r1
 call strcat
+pop r1
 pop r0
-
 ; Check if exists
 label getpathTry
 push r0
+push r1
 mov r0 266
 mov r1 getpathScratchBuf
 syscall
 cmp r2 r0 r0
+pop r1
 pop r0
 skipneqz r2
 jmp getpathMakeAbsolute
-
 ; It does - copy path into dest in r0
 mov r1 getpathScratchBuf
 call strcpy
 ret
-
 ; Otherwise make sure dest path is absolute
 label getpathMakeAbsolute
-mov r1 getpathScratchBuf
 call getabspath
 ret
