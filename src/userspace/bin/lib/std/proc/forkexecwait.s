2a5925f88912ff3a78cf2b9e2762962cbb7f0fc5
DJW <danielwhite94@yahoo.co.uk>
2018-08-15 19:41:34 +0100

    Added forkexecwait asm function

---
+++ src/userspace/bin/lib/std/proc/forkexecwait.s
@@ -0,0 +1,17 @@
+requireend forkexec.s
+
+label forkexecwait ; takes path in r0 to exec in a forked process, with arguments in r1-r3, waiting until it completes before returning in the parent
+; call forkexec to start the child
+call forkexec
+; check for bad fork
+cmp r1 r0 r0
+skipneqz r1
+jmp forkexecwaitret
+; wait for child to die
+mov r1 r0
+mov r0 6
+mov r2 0
+syscall
+; done
+label forkexecwaitret
+ret
