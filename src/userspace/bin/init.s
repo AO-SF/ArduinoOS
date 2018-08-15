4f12e43b93287b76c5a2faba889e14565b93d17c
DJW <dan@fake.com>
2018-08-15 19:31:55 +0100

    init.s now uses new forkexec function

--- src/userspace/bin/init.s
+++ src/userspace/bin/init.s
@@ -1,8 +1,9 @@
 ; Simply execs shell
 
 requireend lib/std/proc/exit.s
+requireend lib/std/proc/forkexec.s
 
-db execPath '/bin/sh', 0
+db shellPath '/bin/sh', 0
 
 ; ensure we have pid 0 (otherwise init should already be running)
 mov r0 1
@@ -11,21 +12,19 @@ cmp r0 r0 r0
 skipeqz r0
 jmp error
 
-; call fork
-mov r0 4
-syscall
+; call forkexec to start shell
+mov r0 shellPath
+mov r1 0
+mov r2 0
+mov r3 0
+call forkexec
 
-mov r1 64 ; PidMax
-cmp r1 r0 r1
-skipneq r1
+; error forking?
+cmp r0 r0 r0
+skipneqz r0
 jmp error
 
-cmp r1 r0 r0
-skipneqz r1
-jmp child
-jmp parent
-
-; call waitpid with our own pid so that we sleep forever
+; otherwise call waitpid with our own pid so that we sleep forever
 label parent
 mov r0 6
 mov r1 0
@@ -33,16 +32,8 @@ mov r2 0 ; infinite timeout
 syscall
 jmp parent ; in case waitpid fails for any reason
 
-; call exec (no arguments)
-label child
-mov r0 5
-mov r1 execPath
-mov r2 0
-mov r3 0
-mov r4 0
-syscall
-
-; exec only returns on failure
+; forkexec error
 label error
 mov r0 1
 call exit
+
