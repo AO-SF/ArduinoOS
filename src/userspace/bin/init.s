5ff2d0677b0f349914884de2a5b70fe790c9e942
DJW <dan@fake.com>
2018-08-20 00:36:14 +0100

    Added suicide signal handler to init so we can shutdown much quicker

--- src/userspace/bin/init.s
+++ src/userspace/bin/init.s
@@ -14,6 +14,12 @@ cmp r0 r0 r0
 skipeqz r0
 jmp error
 
+; Register suicide signal handler
+mov r0 1024
+mov r1 3 ; suicide signal id
+mov r2 suicideHandler
+syscall
+
 ; call forkexecwait to run startup file and wait for it to complete
 mov r0 startupPath
 mov r1 0
@@ -44,4 +50,9 @@ jmp parent ; in case waitpid fails for any reason
 ; forkexec error
 label error
 mov r0 1
+call exit
+
+label suicideHandler
+; For now simply exit
+mov r0 0
 call exit
