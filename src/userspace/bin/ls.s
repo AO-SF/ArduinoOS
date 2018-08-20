09cc49741fa274d04a2af1af58ade6a63fbe2f04
DJW <dan@fake.com>
2018-08-20 23:24:49 +0100

    ls now uses argument if given

--- src/userspace/bin/ls.s
+++ src/userspace/bin/ls.s
@@ -1,35 +1,61 @@
 requireend lib/std/io/fput.s
 requireend lib/std/proc/exit.s
+requireend lib/std/proc/getabspath.s
 requireend lib/std/proc/getpwd.s
 requireend lib/std/str/strlen.s
 
-ab pathBuf 64
-ab pwdFd 1
-ab pwdLen 1
+ab argBuf 64
 
-; Grab pwd and find length
-mov r0 pathBuf
+ab queryDir 64
+ab queryDirFd 1
+ab queryDirLen 1
+
+; Check for argument, otherwise use pwd
+mov r0 3
+mov r1 1
+mov r2 argBuf
+mov r3 64
+syscall
+cmp r0 r0 r0
+skipneqz r0
+jmp noArg
+mov r0 queryDir
+mov r1 argBuf
+call getabspath
+jmp gotArg
+
+label noArg
+mov r0 queryDir
 call getpwd
+label gotArg
 
-mov r0 pathBuf
-call strlen
-mov r1 pwdLen
-store8 r1 r0
-
-; Attempt to open pwd
+; Attempt to open queryDir
 mov r0 258
-mov r1 pathBuf
+mov r1 queryDir
 syscall
 
-mov r1 pwdFd
+mov r1 queryDirFd
 store8 r1 r0
 
 ; Check for bad fd
-mov r0 pwdFd
+mov r0 queryDirFd
 load8 r0 r0
 cmp r0 r0 r0
 skipneqz r0
 jmp error
+
+; Read path back from fd (to handle user passing relative paths for example)
+mov r0 261
+mov r1 queryDirFd
+load8 r1 r1
+mov r2 queryDir
+syscall
+
+; Find queryDir length
+mov r0 queryDir
+call strlen
+mov r1 queryDirLen
+store8 r1 r0
 
 ; Loop calling getChildN and printing results
 mov r2 0
@@ -37,10 +63,10 @@ push r2
 
 label loopStart
 mov r0 260
-mov r1 pwdFd
+mov r1 queryDirFd
 load8 r1 r1
 pop r2
-mov r3 pathBuf
+mov r3 queryDir
 syscall
 inc r2
 push r2
@@ -50,9 +76,9 @@ cmp r0 r0 r0
 skipneqz r0
 jmp success
 
-; Strip pwd from front of child path
-mov r0 pathBuf
-mov r1 pwdLen
+; Strip queryDir from front of child path
+mov r0 queryDir
+mov r1 queryDirLen
 load8 r1 r1
 add r0 r0 r1
 
@@ -74,9 +100,9 @@ label success
 mov r0 '\n'
 call putc0
 
-; Close pwd
+; Close queryDir
 mov r0 259
-mov r1 pwdFd
+mov r1 queryDirFd
 load8 r1 r1
 syscall
 
