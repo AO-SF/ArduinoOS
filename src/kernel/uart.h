c90d2a89d30e02cd474921ada20467e25441872d
DJW <danielwhite94@yahoo.co.uk>
2018-08-23 22:15:48 +0100

    Added uart module

---
+++ src/kernel/uart.h
@@ -0,0 +1,14 @@
+#ifdef ARDUINO
+#ifndef KERNEL_UART_H
+#define KERNEL_UART_H
+
+void uart_putchar(char c, FILE *stream);
+char uart_getchar(FILE *stream);
+
+void uart_init(void);
+
+extern FILE uart_output;
+extern FILE uart_input;
+
+#endif
+#endif
