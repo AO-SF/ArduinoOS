14b19943c7439c8f8c46e0d67bea14764dabe9b3
DJW <danielwhite94@yahoo.co.uk>
2018-08-24 13:20:29 +0100

    Tidied up kernel uart module

--- src/kernel/uart.c
+++ src/kernel/uart.c
@@ -1,6 +1,5 @@
 #ifdef ARDUINO
 #include <avr/io.h>
-#include <stdio.h>
 
 #include <util/setbaud.h>
 
@@ -10,29 +9,27 @@ FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
 FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
 
 void uart_init(void) {
-    UBRR0H = UBRRH_VALUE;
-    UBRR0L = UBRRL_VALUE;
+	UBRR0H = UBRRH_VALUE;
+	UBRR0L = UBRRL_VALUE;
 
-#if USE_2X
-    UCSR0A |= _BV(U2X0);
-#else
-    UCSR0A &= ~(_BV(U2X0));
-#endif
+	#if USE_2X
+		UCSR0A |= _BV(U2X0);
+	#else
+		UCSR0A &= ~(_BV(U2X0));
+	#endif
 
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); // 8-bit data
-    UCSR0B = _BV(RXEN0) | _BV(TXEN0); // Enable RX and TX
+	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); // 8-bit data
+	UCSR0B = _BV(RXEN0) | _BV(TXEN0); // Enable RX and TX
 }
 
 void uart_putchar(char c, FILE *stream) {
-    if (c == '\n')
-        uart_putchar('\r', stream); // TODO: remove?
-    loop_until_bit_is_set(UCSR0A, UDRE0);
-    UDR0 = c;
+	loop_until_bit_is_set(UCSR0A, UDRE0);
+	UDR0=c;
 }
 
 char uart_getchar(FILE *stream) {
-    loop_until_bit_is_set(UCSR0A, RXC0);
-    return UDR0;
+	loop_until_bit_is_set(UCSR0A, RXC0);
+	return UDR0;
 }
 
 #endif
