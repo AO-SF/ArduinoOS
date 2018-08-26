1b756f9ed3c754dd29a417b12d19ef04e09a3a1f
DJW <dan@fake.com>
2018-08-26 22:01:35 +0100

    Added proper arduino millis implementation

--- src/kernel/wrapper.c
+++ src/kernel/wrapper.c
@@ -1,13 +1,9 @@
-// TODO: arduino time stuff here is a horrible hack
-// avr-libc requires init, maintaining and only gives 1s res, need something better
-
 #ifdef ARDUINO
-//#include <time.h>
-#include <stdint.h>
-uint32_t tempTime=0;
-#include <util/delay.h>
+#include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
+#include <stdint.h>
+#include <util/delay.h>
 void *pointerIsHeapBase;
 char vfprintf_PFFormatStr[8];
 #else
@@ -21,8 +17,45 @@ char vfprintf_PFFormatStr[8];
 
 uint32_t kernelBootTime=0;
 
+#ifdef ARDUINO
+#define clockCyclesPerMicrosecond (F_CPU/1000000L)
+#define clockCyclesToMicroseconds(a) ((a)/clockCyclesPerMicrosecond)
+
+// Set prescaler so timer0 ticks every 64 clock cycles, and the overflow handler every 256 ticks.
+#define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(64*256))
+#define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW/1000)
+#define FRACT_INC ((MICROSECONDS_PER_TIMER0_OVERFLOW%1000)>>3)
+#define FRACT_MAX (1000>>3)
+
+volatile unsigned long millisTimerOverflowCount=0;
+volatile unsigned long millisTimerValue=0;
+static unsigned char millisTimerFractional=0;
+
+ISR(TIMER0_OVF_vect) {
+	// Read volatile values
+	unsigned long m=millisTimerValue;
+	unsigned char f=millisTimerFractional;
+
+	m+=MILLIS_INC;
+	f+=FRACT_INC;
+	if (f>=FRACT_MAX) {
+		f-=FRACT_MAX;
+		++m;
+	}
+
+	millisTimerFractional=f;
+	millisTimerValue=m;
+	millisTimerOverflowCount++;
+}
+
+#endif
+
 void millisInit(void) {
 #ifdef ARDUINO
+	// Set timer 0 prescale factor to 64 and enable overflow interrupt
+	TCCR0B|=(1u<<CS01);
+	TCCR0B|=(1u<<CS00);
+	TIMSK0|=(1u<<TOIE0);
 #else
 	// On the Arduino we can leave this at 0 but otherwise we have to save offset
 	kernelBootTime=millisRaw();
@@ -32,8 +65,12 @@ void millisInit(void) {
 
 uint32_t millisRaw(void) {
 	#ifdef ARDUINO
-	//return time(NULL)*((uint32_t)1000);
-	return 100*(tempTime++);
+	uint32_t ms;
+	uint8_t oldSReg=SREG;
+	cli();
+	ms=millisTimerValue;
+	SREG=oldSReg;
+	return ms;
 	#else
 	struct timeval tv;
 	gettimeofday(&tv, NULL); // TODO: Check return value.
