0dceba34b83d7187eb685dd58814d5f08d892e49
DJW <danielwhite94@yahoo.co.uk>
2018-08-26 23:33:01 +0100

    Added kstr to makefile and redesigned it to support progmem strings (at the expense of being 4 bytes not 2)

--- src/kernel/kstr.h
+++ src/kernel/kstr.h
@@ -1,13 +1,30 @@
 #ifndef KSTR_H
 #define KSTR_H
 
-typedef char * KStr;
+#include <stdint.h>
+#ifdef ARDUINO
+#include <avr/pgmspace.h>
+#endif
 
+#define KStrTypeInvalid 0
+#define KStrTypeProgmem 1
+#define KStrTypeStatic 2
+#define KStrTypeHeap 3
+
+typedef struct {
+	uint32_t type:8;
+	uint32_t ptr:24;
+} KStr;
+
+#ifdef ARDUINO
+#define kstrAllocProgmem(str) ({static const char _kstrAllocProgmemStr[] PROGMEM = (str); kstrAllocProgmemRaw(_kstrAllocProgmemStr); })
+KStr kstrAllocProgmemRaw(uint_farptr_t progmemAddr);
+#else
+#define kstrAllocProgmemRaw(src) kstrallocCopy(src);
+#endif
 KStr kstrAllocStatic(char *staticBuffer);
-KStr kstrallocCopy(const char *src);
+KStr kstrAllocCopy(const char *src);
 
-void kstrFree(KStr str);
-
-char *kstrGetC(KStr str);
+void kstrFree(KStr *str);
 
 #endif
