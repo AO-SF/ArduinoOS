7a91f603bac07c1d0a49bd8f13d5e4a4371c6ed9
DJW <dan@fake.com>
2018-08-26 21:50:04 +0100

    Implemented kernel pinRead

--- src/kernel/pins.c
+++ src/kernel/pins.c
@@ -6,6 +6,7 @@
 #include "pins.h"
 
 #define PinsGroupMax 12
+volatile uint8_t *pinsArrayPin[PinsGroupMax]={&PINA, &PINB, &PINC, &PIND, &PINE, &PINF, &PING, &PINH, NULL, &PINJ, &PINK, &PINL};
 volatile uint8_t *pinsArrayDdr[PinsGroupMax]={&DDRA, &DDRB, &DDRC, &DDRD, &DDRE, &DDRF, &DDRG, &DDRH, NULL, &DDRJ, &DDRK, &DDRL};
 volatile uint8_t *pinsArrayPort[PinsGroupMax]={&PORTA, &PORTB, &PORTC, &PORTD, &PORTE, &PORTF, &PORTG, &PORTH, NULL, &PORTJ, &PORTK, &PORTL};
 
@@ -13,8 +14,8 @@ volatile uint8_t *pinsArrayPort[PinsGroupMax]={&PORTA, &PORTB, &PORTC, &PORTD, &
 #define PinNumGetShift(pinNum) ((pinNum)&7)
 
 bool pinRead(uint8_t pinNum) {
-	// TODO: this
-	return false;
+	*pinsArrayDdr[PinNumGetGroup(pinNum)]&=~(1u<<PinNumGetShift(pinNum));
+	return ((*pinsArrayPin[PinNumGetGroup(pinNum)])&(1u<<PinNumGetShift(pinNum)))!=0;
 }
 
 void pinWrite(uint8_t pinNum, bool value) {
