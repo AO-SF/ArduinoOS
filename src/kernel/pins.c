44e0c4b3579c2dd1c9f30f6089ce40a856a32625
DJW <danielwhite94@yahoo.co.uk>
2018-08-26 23:05:23 +0100

    pins module now handles pc wrapper logic also (rather than kernel.c)

--- src/kernel/pins.c
+++ src/kernel/pins.c
@@ -1,17 +1,10 @@
 #ifdef ARDUINO
-
 #include <avr/io.h>
+#endif
 #include <stdlib.h>
 
+#include "log.h"
 #include "pins.h"
-
-#define PinsGroupMax 12
-volatile uint8_t *pinsArrayPin[PinsGroupMax]={&PINA, &PINB, &PINC, &PIND, &PINE, &PINF, &PING, &PINH, NULL, &PINJ, &PINK, &PINL};
-volatile uint8_t *pinsArrayDdr[PinsGroupMax]={&DDRA, &DDRB, &DDRC, &DDRD, &DDRE, &DDRF, &DDRG, &DDRH, NULL, &DDRJ, &DDRK, &DDRL};
-volatile uint8_t *pinsArrayPort[PinsGroupMax]={&PORTA, &PORTB, &PORTC, &PORTD, &PORTE, &PORTF, &PORTG, &PORTH, NULL, &PORTJ, &PORTK, &PORTL};
-
-#define PinNumGetGroup(pinNum) ((pinNum)>>3)
-#define PinNumGetShift(pinNum) ((pinNum)&7)
 
 #define PinNB 96
 const uint8_t pinsValidArray[PinNB]={
@@ -23,22 +16,47 @@ const uint8_t pinsValidArray[PinNB]={
 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 80-95
 };
 
+#ifdef ARDUINO
+#define PinsGroupMax 12
+volatile uint8_t *pinsArrayPin[PinsGroupMax]={&PINA, &PINB, &PINC, &PIND, &PINE, &PINF, &PING, &PINH, NULL, &PINJ, &PINK, &PINL};
+volatile uint8_t *pinsArrayDdr[PinsGroupMax]={&DDRA, &DDRB, &DDRC, &DDRD, &DDRE, &DDRF, &DDRG, &DDRH, NULL, &DDRJ, &DDRK, &DDRL};
+volatile uint8_t *pinsArrayPort[PinsGroupMax]={&PORTA, &PORTB, &PORTC, &PORTD, &PORTE, &PORTF, &PORTG, &PORTH, NULL, &PORTJ, &PORTK, &PORTL};
+
+#define PinNumGetGroup(pinNum) ((pinNum)>>3)
+#define PinNumGetShift(pinNum) ((pinNum)&7)
+
+#else
+
+bool pinStates[PinNB]={0};
+
+#endif
+
 bool pinRead(uint8_t pinNum) {
-	if (pinNum>=PinNB)
+	if (pinNum>=PinNB) {
+		kernelLog(LogTypeWarning, "bad pin %u in read\n", pinNum);
 		return false;
+	}
+#ifdef ARDUINO
 	*pinsArrayDdr[PinNumGetGroup(pinNum)]&=~(1u<<PinNumGetShift(pinNum));
 	return ((*pinsArrayPin[PinNumGetGroup(pinNum)])&(1u<<PinNumGetShift(pinNum)))!=0;
+#else
+	return pinStates[pinNum];
+#endif
 }
 
 bool pinWrite(uint8_t pinNum, bool value) {
-	if (pinNum>=PinNB)
+	if (pinNum>=PinNB) {
+		kernelLog(LogTypeWarning, "bad pin %u in write\n", pinNum);
 		return false;
+	}
+#ifdef ARDUINO
 	*pinsArrayDdr[PinNumGetGroup(pinNum)]|=(1u<<PinNumGetShift(pinNum));
 	if (value!=0)
 		*pinsArrayPort[PinNumGetGroup(pinNum)]|=(1u<<PinNumGetShift(pinNum));
 	else
 		*pinsArrayPort[PinNumGetGroup(pinNum)]&=~(1u<<PinNumGetShift(pinNum));
+#else
+	pinStates[pinNum]=(value!=0);
+#endif
 	return true;
 }
-
-#endif
