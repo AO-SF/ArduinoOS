2a778291d707e8b07cefbca257ee3b2428bdf18d
DJW <dan@fake.com>
2018-08-26 17:07:30 +0100

    Added fprintf_PF and vfprintf_PF as avr libc is missing them

--- src/kernel/wrapper.h
+++ src/kernel/wrapper.h
@@ -3,6 +3,7 @@
 
 #ifdef ARDUINO
 #include <stdbool.h>
+#include <stdio.h>
 extern void *pointerIsHeapBase; // should be set on startup
 #endif
 #include <stdint.h>
@@ -18,6 +19,9 @@ void delay(uint32_t ms);
 
 #ifdef ARDUINO
 bool pointerIsHeap(const void *ptr);
+
+int16_t fprintf_PF(FILE *stream, uint_farptr_t format, ...);
+int16_t vfprintf_PF(FILE *stream, uint_farptr_t format, va_list ap);
 #endif
 
 #endif
