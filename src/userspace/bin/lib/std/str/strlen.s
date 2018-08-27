f8f13db0f89e933a48754ad57da15dd59f1c1b6c
DJW <danielwhite94@yahoo.co.uk>
2018-08-28 00:11:51 +0100

    optimised asm lib strlen function slightly

--- src/userspace/bin/lib/std/str/strlen.s
+++ src/userspace/bin/lib/std/str/strlen.s
@@ -1,24 +1,16 @@
 ; r0=strlen(r0=str addr)
 label strlen
-
-mov r2 0 ; loop index
-
+mov r1 r0 ; str
+mov r0 0 ; length
 label strlenLoopStart
-
-; load character
-mov r3 r0
-add r3 r3 r2
-load8 r3 r3 ; character stored in r3 for now
-
 ; reached null terminator?
-cmp r4 r3 r3
-skipneqz r4
+load8 r2 r1
+cmp r2 r2 r2
+skipneqz r2
 jmp strlenLoopEnd
-
 ; try next character
-inc r2
+inc r0
+inc r1
 jmp strlenLoopStart
-
 label strlenLoopEnd
-mov r0 r2 ; move index into return register
 ret
