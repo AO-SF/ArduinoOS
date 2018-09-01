0fe1676a4c4cf9798f168d46dae6032689eafb45
DJW <dan@fake.com>
2018-09-01 01:40:32 +0100

    Added suicide handler to burn

--- src/userspace/bin/burn.s
+++ src/userspace/bin/burn.s
@@ -1,5 +1,15 @@
-nostack
+; Register suicide signal handler
+mov r0 1024
+mov r1 3 ; suicide signal id
+mov r2 suicideHandler
+syscall
 
 ; Simply loop forever
-label start
-jmp start
+label loop
+jmp loop
+
+; Suicide handler to exit quickly
+label suicideHandler
+mov r0 0
+mov r1 0
+syscall
