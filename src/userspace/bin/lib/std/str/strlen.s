354eb62446518569350b22f31dbbaed82ba3fbb2
DJW <danielwhite94@yahoo.co.uk>
2019-07-17 21:30:09 +0100

    strlen.s library file now uses syscall for speed

--- src/userspace/bin/lib/std/str/strlen.s
+++ src/userspace/bin/lib/std/str/strlen.s
@@ -1,16 +1,15 @@
 ; r0=strlen(r0=str addr)
 label strlen
-mov r1 r0 ; str
-mov r0 0 ; length
-label strlenLoopStart
-; reached null terminator?
-load8 r2 r1
-cmp r2 r2 r2
-skipneqz r2
-jmp strlenLoopEnd
-; try next character
-inc r0
-inc r1
-jmp strlenLoopStart
-label strlenLoopEnd
+; protect string for later subtraction
+push16 r0
+; use strchrnul syscall as the overhead is
+; minimal and kernel space is much faster
+mov r1 r0
+mov r0 1537
+mov r2 0 ; search for a null byte, i.e. the end of the string
+syscall
+; we now have a pointer to the end of the string,
+; so subtract the base address to find length
+pop16 r1
+sub r0 r0 r1
 ret
