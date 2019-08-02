02b022309fb3e1f9a2275edf29d24128f31e09f7
DJW <dan@fake.com>
2019-08-03 00:45:30 +0100

    Fixed bug in asm pinset.s library

--- src/userspace/bin/lib/pin/pinset.s
+++ src/userspace/bin/lib/pin/pinset.s
@@ -6,15 +6,16 @@ label pinset ; num=r0, value=r1, sets pin to value
 push8 r1
 ; open pin device file
 call pinopen
-push8 r0
+push8 r0 ; save fd on stack
 ; set to output
 mov r1 PinModeOutput
 call pinsetmode
 ; write byte
 mov r0 SyscallIdWrite
-pop8 r1
+pop8 r1 ; pop fd off stack
 mov r2 0
-pop8 r3
+dec r6 ; use value on stack
+mov r3 r6
 mov r4 1
 syscall
 ; close pin device file
