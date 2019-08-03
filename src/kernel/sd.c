e28ac7ed22454ccbbf13a900921a3287dfa75fc6
DJW <dan@fake.com>
2019-08-03 22:00:57 +0100

    Began writing SD card kernel module with sdInit written so far.

---
+++ src/kernel/sd.c
@@ -0,0 +1,211 @@
+#include <assert.h>
+#include <stdarg.h>
+
+#include "kernel.h"
+#include "sd.h"
+#include "spi.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// Private prototypes
+////////////////////////////////////////////////////////////////////////////////
+
+void sdWriteCommand(uint8_t firstByte, ...); // terminate with 0xFF, which is not sent
+
+void sdWriteDummyBytes(void); // continues until 0xFF is read
+void sdWriteDummyBytesN(unsigned count);
+
+uint8_t sdWaitForResponse(unsigned max); // Waits until we receive something other than 0xFF, and returns what was read. If not found after max reads, stops and returns 0xFF.
+
+////////////////////////////////////////////////////////////////////////////////
+// Public functions
+////////////////////////////////////////////////////////////////////////////////
+
+SdInitResult sdInit(SdCard *card, uint8_t slaveSelectPin) {
+	uint8_t responseByte;
+	SdInitResult result=SdInitResultOk;
+
+	// Setup card fields
+	card->type=SdTypeBadCard;
+	card->slaveSelectPin=slaveSelectPin;
+	card->addressMode=SdAddressModeByte;
+
+	// Attempt to grab SPI bus lock
+	if (!kernelSpiGrabLockNoSlaveSelect())
+		return SdInitResultSpiLockFail;
+
+	// Ensure MOSI is high initially
+	spiWriteByte(0xFF);
+
+	// We are required to send at least 74 dummy bytes, but do 200 to be safe.
+	sdWriteDummyBytesN(200);
+
+	// Enable the slave by setting pin low
+	pinWrite(slaveSelectPin, false);
+
+	// Send CMD0 - software reset
+	sdWriteCommand(0x40, 0x00, 0x00, 0x00, 0x00, 0x95, 0xFF);
+	responseByte=sdWaitForResponse(16);
+	if (responseByte!=0x01) {
+		result=SdInitResultCouldNotReset;
+		goto error;
+	}
+	sdWriteDummyBytes();
+
+	// Send CMD8 - check voltage range (SDC v2 only)
+	sdWriteCommand(0x48, 0x00, 0x00, 0x01, 0xAA, 0x87, 0xFF);
+	responseByte=sdWaitForResponse(16);
+	if (responseByte!=0x01) {
+		sdWriteDummyBytes();
+
+		// Unhandled case
+		// Should follow up by sending CMD55+ACMD41 (with args 0x00000000 for latter) repeatedly,
+		// until we get either 0x00 indicating SD version 1, or something other than 0x01.
+		// In the latter case try sending CMD1, looking for 0x00, retrying on 0x01, and giving up otherwise.
+
+		result=SdInitResultUnsupportedCard;
+		goto error;
+	}
+
+	// Read 4 byte response for CMD8
+	uint8_t responseR7[4];
+	responseR7[0]=sdWaitForResponse(16);
+	responseR7[1]=sdWaitForResponse(16);
+	responseR7[2]=sdWaitForResponse(16);
+	responseR7[3]=sdWaitForResponse(16);
+	sdWriteDummyBytes();
+
+	// Check if argument was passed back properly.
+	if (responseR7[0]!=0x00 || responseR7[1]!=0x00 || responseR7[2]!=0x01 || responseR7[3]!=0xAA) {
+		// Bad card
+		result=SdInitResultBadCard;
+		goto error;
+	}
+
+	// CMD55+ACMD41 initialise attempt loop
+	unsigned i;
+	for(i=0; i<1024; ++i) { // TODO: think about this constant
+		// Send CMD55 - ACMD command prefix
+		sdWriteCommand(0x77, 0x00, 0x00, 0x00, 0x00, 0x65, 0xFF);
+		responseByte=sdWaitForResponse(16);
+		sdWriteDummyBytes();
+		if (responseByte==0x05) {
+			// Old card - unsupported, use CMD1 similar to case above.
+			result=SdInitResultUnsupportedCard;
+			goto error;
+		} else if (responseByte!=0x01) {
+			// Bad response
+			result=SdInitResultBadCard;
+			goto error;
+		}
+
+		// Send ACMD41 - SDC initialisation
+		sdWriteCommand(0x69, 0x40, 0x00, 0x00, 0x00, 0x77, 0xFF);
+		responseByte=sdWaitForResponse(16);
+		sdWriteDummyBytes();
+
+		if (responseByte==0x01)
+			continue; // give the SD card more time and try again
+		else if (responseByte==0x00)
+			break; // success
+		else {
+			// Bad card
+			result=SdInitResultBadCard;
+			goto error;
+		}
+	}
+
+	if (i==1024) {
+		// Timeout - bad card
+		result=SdInitResultBadCard;
+		goto error;
+	}
+
+	// We now know card's type.
+	card->type=SdTypeSdVer2Plus;
+
+	// Send CMD58 to get card voltage and whether it is extended or not
+	sdWriteCommand(0x7A, 0x00, 0x00, 0x00, 0x00, 0x01, 0xFF);
+	responseByte=sdWaitForResponse(16);
+	if (responseByte!=0x00) {
+		// All cards should support this
+		sdWriteDummyBytes();
+		result=SdInitResultBadCard;
+		goto error;
+	}
+
+	// Read 4 byte response for CMD58
+	uint8_t responseR3[4];
+	responseR3[0]=sdWaitForResponse(16);
+	responseR3[1]=sdWaitForResponse(16);
+	responseR3[2]=sdWaitForResponse(16);
+	responseR3[3]=sdWaitForResponse(16);
+	sdWriteDummyBytes();
+
+	if (responseR3[0]&0x40)
+		card->addressMode=SdAddressModeBlock;
+	else
+		card->addressMode=SdAddressModeByte;
+
+	// If in byte mode, want to send CMD16 to set block size to 512 to match FAT file system.
+	// (block mode devices are fixed at 512 anyway)
+	if (card->addressMode==SdAddressModeByte) {
+		sdWriteCommand(0x50, 0x00, 0x00, 0x02, 0x00, 0x01, 0xFF);
+		responseByte=sdWaitForResponse(16);
+		sdWriteDummyBytes();
+		if (responseByte!=0x00) {
+			// All cards should support this
+			result=SdInitResultBadCard;
+			goto error;
+		}
+	}
+
+	// Release slave select and lock
+	pinWrite(slaveSelectPin, true);
+	kernelSpiReleaseLock();
+
+	assert(result==SdInitResultOk);
+	return result;
+
+	error:
+	assert(result!=SdInitResultOk);
+	pinWrite(slaveSelectPin, true);
+	kernelSpiReleaseLock();
+	return result;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Private functions
+////////////////////////////////////////////////////////////////////////////////
+
+
+void sdWriteCommand(uint8_t firstByte, ...) {
+	va_list ap;
+	va_start(ap, firstByte);
+
+	spiWriteByte(firstByte);
+
+	uint8_t byte;
+	while((byte=(uint8_t)va_arg(ap, int))!=0xFF)
+		spiWriteByte(byte);
+
+	va_end(ap);
+}
+
+void sdWriteDummyBytes(void) {
+	while(spiReadByte()!=0xFF)
+		;
+}
+
+void sdWriteDummyBytesN(unsigned count) {
+	for(unsigned i=0; i<count; ++i)
+		spiWriteByte(0xFF);
+}
+
+uint8_t sdWaitForResponse(unsigned max) {
+	for(unsigned i=0; i<max; ++i) {
+		uint8_t byte=spiReadByte();
+		if (byte!=0xFF)
+			return byte;
+	}
+	return 0xFF;
+}
---
+++ src/kernel/sd.h
@@ -0,0 +1,33 @@
+#ifndef SD_H
+#define SD_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef enum {
+	SdInitResultOk,
+	SdInitResultSpiLockFail, // could not grab SPI bus lock
+	SdInitResultCouldNotReset, // software reset command failed (CMD0)
+	SdInitResultBadCard, // illegal response etc.
+	SdInitResultUnsupportedCard, // potentially a valid card, but currently unsupported
+} SdInitResult;
+
+typedef enum {
+	SdTypeBadCard,
+	SdTypeSdVer2Plus,
+} SdType;
+
+typedef enum {
+	SdAddressModeByte,
+	SdAddressModeBlock,
+} SdAddressMode;
+
+typedef struct {
+	SdType type;
+	uint8_t slaveSelectPin;
+	SdAddressMode addressMode;
+} SdCard;
+
+SdInitResult sdInit(SdCard *card, uint8_t slaveSelectPin);
+
+#endif
