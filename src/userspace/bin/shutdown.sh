254e4910bc298bbe5a587606b943e27ec7f526e3
DJW <dan@fake.com>
2019-08-04 21:39:04 +0100

    Updated startup.sh and shutdown.sh scripts to register and mount an SD card to /dev/sd.

--- src/userspace/bin/shutdown.sh
+++ src/userspace/bin/shutdown.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+
+# Deregister SD card read (unmounting SD card volume automatically, if any).
+spidereg 0
+
+# Unmount user's home directory
 unmount /dev/eeprom
+
+# Done
 exit
--- src/userspace/bin/startup.sh
+++ src/userspace/bin/startup.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+
+# Mount user's home directory from EEPROM
 mount customminifs /dev/eeprom /home
+
+# Register SD card reader in SPI slot 0 and attempt to mount SD card
+spireg 0 2 # register an SD card reader in slot 0
+spisdmnt 0 /dev/sd
+
+# Done
 exit
