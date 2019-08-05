a53d893d3266935fb5a2011a18ba26354bfa0b45
DJW <dan@fake.com>
2019-08-05 02:30:05 +0100

    Added mount/unmount calls to startup/shutdown scripts to mount /dev/sd to /media/sd as a proper file system, not just a raw series of bytes.

--- src/userspace/bin/shutdown.sh
+++ src/userspace/bin/shutdown.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Deregister SD card read (unmounting SD card volume automatically, if any).
+unmount /dev/sd
 spidereg 0
 
 # Unmount user's home directory
--- src/userspace/bin/startup.sh
+++ src/userspace/bin/startup.sh
@@ -6,6 +6,7 @@ mount customminifs /dev/eeprom /home
 # Register SD card reader in SPI slot 0 and attempt to mount SD card
 spireg 0 2 # register an SD card reader in slot 0
 spisdmnt 0 /dev/sd
+mount customminifs /dev/sd /media/sd
 
 # Done
 exit
