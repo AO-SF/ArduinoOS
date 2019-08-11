51f4aa8d8e18975faa79bc1349502eb4a054c9ab
DJW <dan@fake.com>
2019-08-11 17:59:47 +0100

    Minor improvements to tty.s

--- src/userspace/bin/tty.s
+++ src/userspace/bin/tty.s
@@ -5,10 +5,14 @@ requireend lib/std/proc/exit.s
 
 ab pathBuf PathMax
 
-; Grab stdout fd
+; Grab stdout fd and check for invalid fd
 ; TODO: What about stdin?
 mov r0 SyscallIdEnvGetStdoutFd
 syscall
+
+cmp r1 r0 r0
+skipneqz r1
+jmp done
 
 ; Grab path
 mov r1 r0
@@ -16,21 +20,21 @@ mov r0 SyscallIdGetPath
 mov r2 pathBuf
 syscall
 
-; Check return
-; cmp r0 r0 r0
-; skipeqz r0
-; jmp print
-;
-; mov r0 1
-; call exit
+cmp r0 r0 r0
+skipeqz r0
+jmp print
+
+mov r0 1
+call exit
 
 ; Print path
-;label print
+label print
 mov r0 pathBuf
 call puts0
 mov r0 '\n'
 call putc0
 
 ; Exit
+label done
 mov r0 0
 call exit
