bdf8b7e8a96656f4e8768164281b87416f310f6c
DJW <dan@fake.com>
2019-08-15 13:42:20 +0100

    Updated fputdec family of functions to use inttostr followed by puts.

--- src/userspace/bin/lib/std/io/fputdec.s
+++ src/userspace/bin/lib/std/io/fputdec.s
@@ -1,4 +1,5 @@
 require fput.s
+require ../str/inttostr.s
 
 ; putdecpad(x=r0)
 label putdecpad
@@ -21,90 +22,20 @@ mov r2 0
 jmp fputdeccommon
 
 ; fputdeccommon(fd=r0, x=r1, padFlag=r2) - write x in ascii as a decimal value to given fd, optionally padded with zeros
-; note: this was originally done with a loop but it is significantly faster unrolled and optimised
 label fputdeccommon
-; if padding flag set, simply jump straight to printing first digit
-cmp r2 r2 r2
-skipeqz r2
-jmp fputdecprint4
-; check if single digit
-mov r2 10
-cmp r4 r1 r2
-skipge r4
-jmp fputdecprint0
-; check if two digits
-mov r3 100
-cmp r4 r1 r3
-skipge r4
-jmp fputdecprint1
-; check if three digits
-mul r3 r2 r3 ; shorter version of `mov r3 1000`
-cmp r4 r1 r3
-skipge r4
-jmp fputdecprint2
-; check if four digits
-mul r2 r2 r3 ; shorter version of `mov r2 10000`
-cmp r4 r1 r2
-skipge r4
-jmp fputdecprint3
-; else five digits
-; print ten-thousands digit
-label fputdecprint4
-mov r2 10000
-div r3 r1 r2
-mul r4 r3 r2
-sub r1 r1 r4
-push8 r0
-push16 r1
-mov r2 '0'
-add r1 r3 r2
-call fputc0
-pop16 r1
-pop8 r0
-; print thousands digit
-label fputdecprint3
-mov r2 1000
-div r3 r1 r2
-mul r4 r3 r2
-sub r1 r1 r4
-push8 r0
-push16 r1
-mov r2 '0'
-add r1 r3 r2
-call fputc0
-pop16 r1
-pop8 r0
-; print hundreds digit
-label fputdecprint2
-mov r2 100
-div r3 r1 r2
-mul r4 r3 r2
-sub r1 r1 r4
-push8 r0
-push8 r1 ; notice from here onwards we can assume x is <256 so fits in 8 bits
-mov r2 '0'
-add r1 r3 r2
-call fputc0
-pop8 r1
-pop8 r0
-; print tens digit
-label fputdecprint1
-mov r2 10
-div r3 r1 r2
-mul r4 r3 r2
-sub r1 r1 r4
-push8 r0
-push8 r1
-mov r2 '0'
-add r1 r3 r2
-call fputc0
-pop8 r1
-pop8 r0
-; print units digit
-label fputdecprint0
-; divisor is one - nothing to do
-; also no need to protect registers in this case - we are about to return
-mov r2 '0'
-add r1 r1 r2
-call fputc0
+; reserve 6 bytes of stack to store temporary string
+mov r3 r6
+inc6 r6
+; convert x to a string
+push8 r0 ; protect fd
+push16 r3 ; protect str addr
+mov r0 r3
+call inttostr
+; print string
+pop16 r2 ; restore str addr
+mov r1 0
+pop8 r0 ; restore fd
+call fputs
+; restore stack
+dec6 r6
 ret
