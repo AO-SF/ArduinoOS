05d918636be525e0e44e8d8bc4775e5c531a2058
DJW <dan@fake.com>
2019-09-14 23:03:32 +0100

    hwreg user space utility now uses human readable strings for type argument instead of raw number

--- src/userspace/bin/hwreg.s
+++ src/userspace/bin/hwreg.s
@@ -2,9 +2,14 @@ require lib/sys/sys.s
 
 requireend lib/std/io/fput.s
 requireend lib/std/proc/exit.s
+requireend lib/std/str/strequal.s
 requireend lib/std/str/strtoint.s
 
 db usageStr 'usage: id type\n',0
+db badTypeStr 'Bad type\n',0
+db typeStrRaw 'raw',0
+db typeStrSdCardReader 'sdcardreader',0
+db typeStrDht22 'dht22',0
 
 ab argBuf ArgLenMax
 
@@ -33,11 +38,43 @@ jmp usage ; id is not popped from stack but no harm
 
 ; Convert type arg to integer
 mov r0 argBuf
-call strtoint
+mov r1 typeStrRaw
+call strequal
+cmp r0 r0 r0
+skipeqz r0
+jmp typeIsRaw
+
+mov r0 argBuf
+mov r1 typeStrSdCardReader
+call strequal
+cmp r0 r0 r0
+skipeqz r0
+jmp typeIsSdCardReader
+
+mov r0 argBuf
+mov r1 typeStrDht22
+call strequal
+cmp r0 r0 r0
+skipeqz r0
+jmp typeIsDht22
+
+jmp badType
+
+label typeIsRaw
+mov r2 SyscallHwDeviceTypeRaw
+jmp doSyscall
+
+label typeIsSdCardReader
+mov r2 SyscallHwDeviceTypeSdCardReader
+jmp doSyscall
+
+label typeIsDht22
+mov r2 SyscallHwDeviceTypeDht22
+jmp doSyscall
 
 ; Use syscall to register device
-mov r2 r0
-pop8 r1
+label doSyscall
+pop8 r1 ; pop desired id/slot off stack
 mov r0 SyscallIdHwDeviceRegister
 syscall
 
@@ -47,6 +84,12 @@ call exit
 
 label usage
 mov r0 usageStr
+call puts0
+mov r0 1
+call exit
+
+label badType
+mov r0 badTypeStr
 call puts0
 mov r0 1
 call exit
--- src/userspace/bin/startup.sh
+++ src/userspace/bin/startup.sh
@@ -4,7 +4,7 @@
 mount customminifs /dev/eeprom /home
 
 # Register SD card reader in HW device slot 0 and attempt to mount SD card
-hwreg 0 2 # register an SD card reader in slot 0
+hwreg 0 sdcardreader # register an SD card reader in slot 0
 hwsdmnt 0 /dev/sd
 mount customminifs /dev/sd /media/sd
 
