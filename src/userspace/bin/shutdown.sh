d171f8fbc3badf23919ce4e3bf73d21bd2f682a1
DJW <dan@fake.com>
2019-09-16 21:28:59 +0100

    Restructured startup.sh and shutdown.sh to speed up booting.

--- src/userspace/bin/shutdown.sh
+++ src/userspace/bin/shutdown.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-# Deregister SD card read (unmounting SD card volume automatically, if any).
-unmount /dev/sd
-hwdereg 0
-
 # Unmount user's home directory
 unmount /dev/eeprom
 
 # Done
 exit
+
+# Example Code
+
+# Deregister SD card read (unmounting SD card volume automatically, if any).
+unmount /dev/sd
+hwdereg 0
--- src/userspace/bin/startup.sh
+++ src/userspace/bin/startup.sh
@@ -3,10 +3,12 @@
 # Mount user's home directory from EEPROM
 mount customminifs /dev/eeprom /home
 
+# Done
+exit
+
+# Example Code
+
 # Register SD card reader in HW device slot 0 and attempt to mount SD card
 hwreg 0 sdcardreader # register an SD card reader in slot 0
 hwsdmnt 0 /dev/sd
 mount customminifs /dev/sd /media/sd
-
-# Done
-exit
