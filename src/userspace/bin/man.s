10868eea3c8292e94f1716034e9fe2879764b9e6
DJW <dan@fake.com>
2019-09-16 16:34:32 +0100

    Updated man utility to handle new exec format

--- src/userspace/bin/man.s
+++ src/userspace/bin/man.s
@@ -18,12 +18,13 @@ ab sectionArgBuf ArgLenMax
 ab pageArgBuf ArgLenMax
 
 ab pathBuf ArgLenMax
+ab argvBuffer ArgLenMax ; TODO: should be ArgLenMax+5 at least? (for 'cat '), can also avoid using two buffers
 
 ; Register simple suicide handler
 require lib/std/proc/suicidehandler.s
 
 ; Get args
-mov r0 SyscallIdArgvN
+mov r0 SyscallIdArgvN ; grab section number as string
 mov r1 1
 mov r2 sectionArgBuf
 mov r3 ArgLenMax
@@ -31,7 +32,8 @@ syscall
 cmp r0 r0 r0
 skipneqz r0
 jmp usage
-mov r0 SyscallIdArgvN
+
+mov r0 SyscallIdArgvN ; get page string
 mov r1 2
 mov r2 pageArgBuf
 mov r3 ArgLenMax
@@ -41,16 +43,16 @@ skipneqz r0
 jmp usage
 
 ; Create path where manual will exist if it does
-mov r0 pathBuf
+mov r0 pathBuf ; start with base path
 mov r1 basePath
 call strcpy
-mov r0 pathBuf
+mov r0 pathBuf ; then add section number
 mov r1 sectionArgBuf
 call strcat
-mov r0 pathBuf
+mov r0 pathBuf ; then add a slash
 mov r1 slashStr
 call strcat
-mov r0 pathBuf
+mov r0 pathBuf ; finally add page
 mov r1 pageArgBuf
 call strcat
 
@@ -62,11 +64,19 @@ cmp r0 r0 r0
 skipneqz r0
 jmp notFound
 
-; exec to cat to display file
-mov r0 catPath
+; create argv for exec call to cat
+mov r0 argvBuffer ; start with 'cat' with a terminating null byte
+mov r1 catPath
+call strcpy
+
+mov r0 argvBuffer ; append path as argument
+inc4 r0
 mov r1 pathBuf
-mov r2 emptyStr
-mov r3 emptyStr
+call strcpy
+
+; exec to cat to display file
+mov r0 2
+mov r1 argvBuffer
 call runpath
 
 ; Exec only fails on error
