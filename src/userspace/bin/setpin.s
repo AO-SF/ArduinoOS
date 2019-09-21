b2ae629c9a9ca4507197c21e49163b7d1443d8aa
DJW <dan@fake.com>
2019-09-21 15:49:15 +0100

    setpin user space utility now checks pin number is valid before attempting to set value

--- src/userspace/bin/setpin.s
+++ src/userspace/bin/setpin.s
@@ -1,6 +1,7 @@
 require lib/sys/sys.s
 
 requireend lib/pin/pinset.s
+requireend lib/pin/pinvalid.s
 requireend lib/pin/strtopin.s
 requireend lib/std/io/fput.s
 requireend lib/std/proc/exit.s
@@ -32,16 +33,17 @@ jmp usage
 mov r0 pinArgBuf
 call strtopin
 
-mov r1 PinInvalid
-cmp r1 r0 r1
-skipneq r1
-jmp badPin
+push8 r0 ; protect pinNum
+call pinvalid
+cmp r0 r0 r0
+skipneqz r0
+jmp badPin ; no need to restore stack as we will exit very soon
 
-push8 r0
 mov r0 stateArgBuf
 call strtoint
 mov r1 r0
-pop8 r0
+
+pop8 r0 ; restore pinNum
 
 ; Use pin library to set state
 call pinset
