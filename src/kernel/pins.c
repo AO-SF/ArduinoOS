2e7a5b6229a311d08b2008ac1135a3edf329a03d
DJW <dan@fake.com>
2019-09-21 12:57:08 +0100

    Refactored pins.c in various ways, mainly by grouping pin data into a struct per group.

--- src/kernel/pins.c
+++ src/kernel/pins.c
@@ -7,27 +7,53 @@
 #include "pins.h"
 #include "util.h"
 
-const uint8_t pinsArrayValid[PinGroupMax]={
-	0xFF, 0xFF, //  0-15
-	0xFF, 0x8F, // 16-31
-	0x3B, 0xFF, // 32-47
-	0x27, 0x7B, // 48-63
-	0x00, 0x03, // 64-79
-	0xFF, 0xFF, // 80-95
+typedef struct {
+	uint8_t valid;
+#ifdef ARDUINO
+	volatile uint8_t *pin;
+	volatile uint8_t *ddr;
+	volatile uint8_t *port;
+#endif
+} PinsGroupData;
+
+PinsGroupData pinsGroupData[PinGroupMax]={
+#ifdef ARDUINO
+	{.valid=0xFF, .pin=&PINA, .ddr=&DDRA, .port=&PORTA},
+	{.valid=0xFF, .pin=&PINB, .ddr=&DDRB, .port=&PORTB},
+	{.valid=0xFF, .pin=&PINC, .ddr=&DDRC, .port=&PORTC},
+	{.valid=0x8F, .pin=&PIND, .ddr=&DDRD, .port=&PORTD},
+	{.valid=0x3B, .pin=&PINE, .ddr=&DDRE, .port=&PORTE},
+	{.valid=0xFF, .pin=&PINF, .ddr=&DDRF, .port=&PORTF},
+	{.valid=0x27, .pin=&PING, .ddr=&DDRG, .port=&PORTG},
+	{.valid=0x7B, .pin=&PINH, .ddr=&DDRH, .port=&PORTH},
+	{.valid=0x00, .pin=NULL , .ddr=NULL , .port=NULL  },
+	{.valid=0x03, .pin=&PINJ, .ddr=&DDRJ, .port=&PORTJ},
+	{.valid=0xFF, .pin=&PINK, .ddr=&DDRK, .port=&PORTK},
+	{.valid=0xFF, .pin=&PINL, .ddr=&DDRL, .port=&PORTL},
+#else
+	{.valid=0xFF},
+	{.valid=0xFF},
+	{.valid=0xFF},
+	{.valid=0x8F},
+	{.valid=0x3B},
+	{.valid=0xFF},
+	{.valid=0x27},
+	{.valid=0x7B},
+	{.valid=0x00},
+	{.valid=0x03},
+	{.valid=0xFF},
+	{.valid=0xFF},
+#endif
 };
 
-#ifdef ARDUINO
-volatile uint8_t *pinsArrayPin[PinGroupMax]={&PINA, &PINB, &PINC, &PIND, &PINE, &PINF, &PING, &PINH, NULL, &PINJ, &PINK, &PINL};
-volatile uint8_t *pinsArrayDdr[PinGroupMax]={&DDRA, &DDRB, &DDRC, &DDRD, &DDRE, &DDRF, &DDRG, &DDRH, NULL, &DDRJ, &DDRK, &DDRL};
-volatile uint8_t *pinsArrayPort[PinGroupMax]={&PORTA, &PORTB, &PORTC, &PORTD, &PORTE, &PORTF, &PORTG, &PORTH, NULL, &PORTJ, &PORTK, &PORTL};
-#else
-bool pinStates[PinMax]={0};
+#ifndef ARDUINO
+bool pcWrapperVirtualPinStates[PinMax]={0};
 #endif
 
 bool pinIsValid(uint8_t pinNum) {
 	if (pinNum>=PinMax)
 		return false;
-	return (pinsArrayValid[PinNumGetGroup(pinNum)]>>PinNumGetShift(pinNum))&1;
+	return (pinsGroupData[PinNumGetGroup(pinNum)].valid>>PinNumGetShift(pinNum))&1;
 }
 
 void pinSetMode(uint8_t pinNum, PinMode mode) {
@@ -38,14 +64,15 @@ void pinSetMode(uint8_t pinNum, PinMode mode) {
 #ifdef ARDUINO
 	switch(mode) {
 		case PinModeInput:
-			*pinsArrayDdr[PinNumGetGroup(pinNum)]&=~(1u<<PinNumGetShift(pinNum));
+			*pinsGroupData[PinNumGetGroup(pinNum)].ddr&=~(1u<<PinNumGetShift(pinNum));
 		break;
 		case PinModeOutput:
-			*pinsArrayDdr[PinNumGetGroup(pinNum)]|=(1u<<PinNumGetShift(pinNum));
+			*pinsGroupData[PinNumGetGroup(pinNum)].ddr|=(1u<<PinNumGetShift(pinNum));
 		break;
 	}
+#else
+	// Nothing to do in PC version
 #endif
-// Null-op on PC
 }
 
 bool pinRead(uint8_t pinNum) {
@@ -54,9 +81,9 @@ bool pinRead(uint8_t pinNum) {
 		return false;
 	}
 #ifdef ARDUINO
-	return ((*pinsArrayPin[PinNumGetGroup(pinNum)])&(1u<<PinNumGetShift(pinNum)))!=0;
+	return ((*pinsGroupData[PinNumGetGroup(pinNum)].pin)&(1u<<PinNumGetShift(pinNum)))!=0;
 #else
-	return pinStates[pinNum];
+	return pcWrapperVirtualPinStates[pinNum];
 #endif
 }
 
@@ -67,11 +94,11 @@ bool pinWrite(uint8_t pinNum, bool value) {
 	}
 #ifdef ARDUINO
 	if (value!=0)
-		*pinsArrayPort[PinNumGetGroup(pinNum)]|=(1u<<PinNumGetShift(pinNum));
+		*pinsGroupData[PinNumGetGroup(pinNum)].port|=(1u<<PinNumGetShift(pinNum));
 	else
-		*pinsArrayPort[PinNumGetGroup(pinNum)]&=~(1u<<PinNumGetShift(pinNum));
+		*pinsGroupData[PinNumGetGroup(pinNum)].port&=~(1u<<PinNumGetShift(pinNum));
 #else
-	pinStates[pinNum]=(value!=0);
+	pcWrapperVirtualPinStates[pinNum]=(value!=0);
 #endif
 	return true;
 }
