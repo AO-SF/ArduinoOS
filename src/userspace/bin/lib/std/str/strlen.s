10ca7217dec6fd0e547be15030febe358b5af725
DJW <dan@fake.com>
2019-10-04 20:25:15 +0100

    strlen asm library function no longer needlessly uses the stack (i.e. ram)

--- src/userspace/bin/lib/std/str/strlen.s
+++ src/userspace/bin/lib/std/str/strlen.s
@@ -3,7 +3,7 @@ require ../../sys/syscall.s
 ; r0=strlen(r0=str addr)
 label strlen
 ; protect string for later subtraction
-push16 r0
+mov r3 r0
 ; use strchrnul syscall as the overhead is
 ; minimal and kernel space is much faster
 mov r1 r0
@@ -12,6 +12,5 @@ mov r2 0 ; search for a null byte, i.e. the end of the string
 syscall
 ; we now have a pointer to the end of the string,
 ; so subtract the base address to find length
-pop16 r1
-sub r0 r0 r1
+sub r0 r0 r3
 ret
