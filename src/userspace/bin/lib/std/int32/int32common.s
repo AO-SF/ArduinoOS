f6bb4ab8cabff909470b17bc0d88b35245790df7
DJW <dan@fake.com>
2019-10-13 15:26:45 +0100

    Added various constants to int32common.s user space library

--- src/userspace/bin/lib/std/int32/int32common.s
+++ src/userspace/bin/lib/std/int32/int32common.s
@@ -1,1 +1,17 @@
-aw int32ScratchInt32 2 ; scratch 32 bit value used by various functions
+; Various useful constants
+dw Int32Const0 0,0
+dw Int32Const1 0,1
+
+; Powers of ten
+dw Int32Const1E1 0,10
+dw Int32Const1E2 0,100
+dw Int32Const1E3 0,1000 ; 10^3
+dw Int32Const1E4 0,10000
+dw Int32Const1E5 1,34464
+dw Int32Const1E6 15,16960 ; 10^6
+dw Int32Const1E7 152,38528
+dw Int32Const1E8 1525,57600
+dw Int32Const1E9 15258,51712 ; 10^9
+
+; Scratch 32 bit value used by various functions
+aw int32ScratchInt32 2
