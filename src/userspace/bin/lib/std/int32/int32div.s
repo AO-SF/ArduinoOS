9af8e75bc9736e414ccbd431a73f3ddfa6ba75c7
DJW <dan@fake.com>
2019-10-18 23:05:21 +0100

    Made 32 bit division function for user space int 32 asm lib

--- src/userspace/bin/lib/std/int32/int32div.s
+++ src/userspace/bin/lib/std/int32/int32div.s
@@ -1,33 +1,59 @@
-; int32div16(r0=dest, r1=divisor) - divide 32 bit quantity at r0 by 16 bit divisor in r1
+requireend int32add.s
+requireend int32cmp.s
+requireend int32sub.s
+
+aw int32divScratchInt32 2
+
+; int32div16(r0=dest, r1=d) - where dest/src is ptr to 32 bit value, and d is 16 bit divsor
 label int32div16
+; Use stack to store a 32 bit version of the given divisor
+mov r2 r6
+inc4 r6
+push16 r0 ; protect dest ptr
+push16 r2 ; protect 32 bit divisor ptr
+mov r0 r2
+call int32set16
+pop16 r1 ; restore divisor ptr
+pop16 r0 ; restore dest ptr
+; Call standard 32 bit division function
+call int32div
+; Restore stack
+dec4 r6
+ret
+
+; int32div(r0=dest, r1=divisor) - divide 32 bit quantity at r0 by 32 bit divisor at r1
+label int32div
 ; TODO: Use a better algorithm
-mov r2 0 ; result counter
-label int32div16loopstart
+; Use scratch int32 as result counter, initially set to 0
+push16 r0
+push16 r1
+mov r0 int32divScratchInt32
+mov r1 0
+call int32set16
+pop16 r1
+pop16 r0
+label int32divloopstart
 ; Is the value smaller than the divisor?
-load16 r3 r0
-cmp r3 r3 r3
-skipeqz r3
-jmp int32div16loopnext
-inc2 r0
-load16 r3 r0
-dec2 r0
-cmp r3 r3 r1
-skipge r3
-jmp int32div16loopend
-label int32div16loopnext
+push16 r0
+push16 r1
+call int32LessThan
+cmp r2 r0 r0
+pop16 r1
+pop16 r0
+skipeqz r2
+jmp int32divloopend
 ; Subtract divisor from dest
 push16 r0
 push16 r1
-push16 r2
-call int32sub16
-pop16 r2
+call int32sub32
+; Inc result counter and loop again
+mov r0 int32divScratchInt32
+call int32inc
 pop16 r1
 pop16 r0
-; Inc result counter and loop again
-inc r2
-jmp int32div16loopstart
-; Done
-label int32div16loopend
-mov r1 r2
-call int32set16
+jmp int32divloopstart
+; Done - copy result value into dest
+label int32divloopend
+mov r1 int32divScratchInt32
+call int32set32
 ret
