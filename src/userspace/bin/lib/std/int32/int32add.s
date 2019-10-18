d94ac6d6da355680c6489da180c25a2cbb86efed
DJW <dan@fake.com>
2019-10-18 23:04:42 +0100

    Added int32inc function for user space int 32 asm lib

--- src/userspace/bin/lib/std/int32/int32add.s
+++ src/userspace/bin/lib/std/int32/int32add.s
@@ -1,3 +1,8 @@
+; int32inc(dest=r0)=int32add16(dest,1) - increment dest
+label int32inc
+mov r1 1
+jmp int32add16 ; this function will return for us
+
 ; int32add16(dest=r0, src=r1)
 label int32add16
 ; Compute maximum we can add to dest.lower before overflowing
