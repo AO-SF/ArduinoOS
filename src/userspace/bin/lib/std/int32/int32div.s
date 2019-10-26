4ae13acce78d39b75f13a84da73b838fa368dc63
DJW <dan@fake.com>
2019-10-26 23:51:31 +0100

    Added int32div32rem and based int32div32 on it

--- src/userspace/bin/lib/std/int32/int32div.s
+++ src/userspace/bin/lib/std/int32/int32div.s
@@ -4,7 +4,7 @@ requireend int32sub.s
 
 aw int32divScratchInt32 2
 
-; int32div16(r0=dest, r1=d) - where dest/src is ptr to 32 bit value, and d is 16 bit divsor
+; int32div16(r0=x, r1=d) - computes x=x/d where x is ptr to 32 bit value, and d is 16 bit divisor
 label int32div16
 ; Use stack to store a 32 bit version of the given divisor
 mov r2 r6
@@ -23,7 +23,21 @@ ret
 
 ; int32div32(r0=dest, r1=divisor) - divide 32 bit quantity at r0 by 32 bit divisor at r1
 label int32div32
+; We will use int32div32rem to do most of the work
+; So first reserve a dummy 32 bit int on the stack to act as the remainder argument
+mov r2 r6
+inc4 r6
+; Call int32div32rem to do the division
+call int32div32rem
+; Restore stack
+dec4 r6
+ret
+
+; int32div32rem(r0=dest, r1=divisor, r2=remainder) - divide 32 bit quantity at r0 by 32 bit divisor at r1, setting remainder in the process
+label int32div32rem
 ; TODO: Use a better algorithm
+; Protect remainder for the end of the function
+push16 r2
 ; Use scratch int32 as result counter, initially set to 0
 push16 r0
 push16 r1
@@ -52,8 +66,13 @@ call int32inc
 pop16 r1
 pop16 r0
 jmp int32div32loopstart
-; Done - copy result value into dest
+; Done - set remainder and copy result value into dest
 label int32divloopend
+mov r1 r0
+pop16 r0 ; restore remainder pointer
+push16 r1
+call int32set32
+pop16 r0
 mov r1 int32divScratchInt32
 call int32set32
 ret
