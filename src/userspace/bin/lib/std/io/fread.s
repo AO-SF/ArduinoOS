a86c00d60729eab417f41c5b14b734f3bc03582f
DJW <dan@fake.com>
2019-11-25 23:25:07 +0000

    Added fread32 user space asm function

--- src/userspace/bin/lib/std/io/fread.s
+++ src/userspace/bin/lib/std/io/fread.s
@@ -10,3 +10,14 @@ mov r1 r0
 mov r0 SyscallIdRead
 syscall
 ret
+
+; readCount=fread32(fd=r0, offsetPtr=r1, data=r2, dataLen=r3 - version of fread with 32 bit offset argument, places number of bytes read into r0
+label fread32
+; Use read32 syscall
+mov r4 r3
+mov r3 r2
+mov r2 r1
+mov r1 r0
+mov r0 SyscallIdRead32
+syscall
+ret
