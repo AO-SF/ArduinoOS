e2329562019d8953345e870742819aeaeeef73fa
DJW <asd@example.com>
2020-04-13 22:31:31 +0100

    Updated userspace MaxFds constant to 9 instead of 8

--- src/userspace/bin/lib/sys/proc.s
+++ src/userspace/bin/lib/sys/proc.s
@@ -1,5 +1,5 @@
 const PidMax 16
 const ArgLenMax 64
 const EnvVarPathMax 128
-const MaxFds 8 ; maximum amount of open files a single process can have
+const MaxFds 9 ; maximum amount of open files a single process can have (although local fd=0 is used for invalid so there are only 8 available for us)
 const ArgMax 255
