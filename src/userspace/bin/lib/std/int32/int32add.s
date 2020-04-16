e4d631a4fd0f7d6db644b862ecce0a951a7c1fce
DJW <asd@example.com>
2020-04-16 23:30:44 +0100

    Updated userspace int32 library to use syscalls where possible for speed

--- src/userspace/bin/lib/std/int32/int32add.s
+++ src/userspace/bin/lib/std/int32/int32add.s
@@ -1,65 +1,18 @@
+require ../../sys/syscall.s
+
 ; int32inc(dest=r0)=int32add16(dest,1) - increment dest
 label int32inc
-; check if lower part will overflow
-inc2 r0
-load16 r1 r0
-mov r2 65535
-cmp r2 r1 r2
-skipneq r2
-jmp int32incOverflow
-; standard case - just increment lower
-inc r1
-store16 r0 r1
-ret
-; overflow case - set lower to 0 and inc upper
-label int32incOverflow
-mov r1 0
-store16 r0 r1
-dec2 r0
-load16 r1 r0
-inc r1
-store16 r0 r1
-ret
+mov r1 1
+jmp int32add16 ; this function can return for us
 
 ; int32add16(dest=r0, src=r1)
 label int32add16
-; Compute maximum we can add to dest.lower before overflowing
-inc2 r0
-load16 r2 r0
-dec2 r0
-mov r3 65535
-sub r2 r3 r2 ; r2 has maximum we can add before overflow
-cmp r3 r1 r2
-skipgt r3
-jmp int32add16finaladd
-; We cannot add without overflowing, so add what we can, then add one more,
-; overflowing (inc dest.upper, set dest.lower=0, and reduce src)
-sub r1 r1 r2
-dec r1
-load16 r2 r0
-inc r2
-store16 r0 r2
-inc2 r0
-mov r2 0
-store16 r0 r2
-dec2 r0
-; Add without overflow (including remaining part if there would have been an overflow)
-label int32add16finaladd
-inc2 r0
-load16 r2 r0
-add r2 r2 r1
-store16 r0 r2
 ret
 
 ; int32add32(dest=r0, opA=r1)
 label int32add32
-; Add upper part (not worrying about overflow)
-load16 r2 r0
-load16 r3 r1
-add r2 r2 r3
-store16 r0 r2
-; Add lower part (which may overflow into upper part)
-inc2 r1
-load16 r1 r1
-call int32add16
+mov r2 r1
+mov r1 r0
+mov r0 SyscallIdInt32Add32
+syscall
 ret
--- src/userspace/bin/lib/std/int32/int32sub.s
+++ src/userspace/bin/lib/std/int32/int32sub.s
@@ -1,68 +1,17 @@
+require ../../sys/syscall.s
+
 ; int32sub16(dest=r0, opA=r1)
 label int32sub16
-; Max we can subtract naively is dest lower, so see if this is enough
-inc2 r0
-load16 r2 r0 ; r2 contains dest lower, the max we can subtract initially
-dec2 r0
-cmp r3 r1 r2
-skipgt r3
-jmp int32sub16finalsub
-; We can not subtract everything in one go, reduce opA by amount we can handle currently
-sub r1 r1 r2
-; set dest lower to 2^16-1
-mov r2 65535
-inc2 r0
-store16 r0 r2
-dec2 r0
-; decrement dest upper
-load16 r2 r0
-dec r2
-store16 r0 r2
-label int32sub16finalsub
-inc2 r0
-load16 r2 r0
-sub r2 r2 r1
-store16 r0 r2
+mov r2 r1
+mov r1 r0
+mov r0 SyscallIdInt32Sub16
+syscall
 ret
 
 ; int32sub32(dest=r0, opA=r1) - performs dest=dest-opA, where both arguments are pointers to 32 bit values
 label int32sub32
-; Are we able to subtract lower parts,
-; or do we need to borrow/carry from upper?
-inc2 r0
-inc2 r1
-load16 r2 r0
-load16 r3 r1
-cmp r4 r2 r3
-skiplt r4
-jmp int32sub32NoBorrow
-; Need to borrow/carry from upper part of dest
-; Note: instead of doing (64k+dest.lower)-opA.lower -
-; which would overflow our 16 bit registers -
-; we do (64k-opA.lower)+dest.lower
-mov r4 65535
-dec r3
-xor r4 r3 r4 ; calculate 64k-opA.lower
-add r4 r4 r2 ; add dest.lower
-store16 r0 r4 ; update dest.lower
-; Also decrement dest.upper
-dec2 r0
-load16 r2 r0
-dec r2
-store16 r0 r2
-jmp int32sub32UpperHalves
-; No need to borrow - dest.lower is at least opA.lower,
-; so simply subtract lower halves
-label int32sub32NoBorrow
-sub r4 r2 r3
-store16 r0 r4
-; Move onto upper halves (nothing to borrow from here)
-dec2 r0
-label int32sub32UpperHalves
-dec2 r1
-load16 r2 r0
-load16 r3 r1
-sub r4 r2 r3
-store16 r0 r4
-; Done
+mov r2 r1
+mov r1 r0
+mov r0 SyscallIdInt32Sub32
+syscall
 ret
