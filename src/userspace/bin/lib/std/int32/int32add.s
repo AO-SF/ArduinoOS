da3f70698da91b3dda59d9736fc55552c1c34563
DJW <asd@example.com>
2020-04-17 20:16:10 +0100

    Added missing code to userspace int32add16 function

--- src/userspace/bin/lib/std/int32/int32add.s
+++ src/userspace/bin/lib/std/int32/int32add.s
@@ -7,6 +7,10 @@ jmp int32add16 ; this function can return for us
 
 ; int32add16(dest=r0, src=r1)
 label int32add16
+mov r2 r1
+mov r1 r0
+mov r0 SyscallIdInt32Add16
+syscall
 ret
 
 ; int32add32(dest=r0, opA=r1)
