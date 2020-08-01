da22da132268ede87e56634b7c9f7c18972b4a40
DJW <asd@example.com>
2020-08-01 01:40:47 +0100

    Optimised cp.s userspace utility to reduce amount of buffers needed (saving 3*64=192 bytes of ram)

--- src/userspace/bin/cp.s
+++ src/userspace/bin/cp.s
@@ -13,12 +13,9 @@ db usageStr 'usage: cp SOURCE DEST\n', 0
 db errorStrBadSource 'could not open source\n', 0
 db errorStrBadDest 'could not create/open dest\n', 0
 
-ab sourceArg PathMax
-ab destArg PathMax
+ab buf PathMax
 ab sourceFd 1
 ab destFd 1
-
-ab copyBuf PathMax
 
 aw fileOffset 2 ; 32 bit integer
 
@@ -34,8 +31,24 @@ skipneqz r1
 jmp showUsage
 
 mov r1 r0
-mov r0 sourceArg
+mov r0 buf
 call getpath
+
+; Open source file
+mov r0 buf
+mov r1 FdModeRO
+call openpath
+cmp r1 r0 r0
+skipneqz r1
+jmp couldNotOpenSource
+mov r1 sourceFd
+store8 r1 r0
+
+; Grab size of source
+mov r0 SyscallIdGetFileLen32
+mov r1 buf
+mov r2 fileOffset
+syscall
 
 ; Get dest argument and call getpath on it
 mov r0 SyscallIdArgvN
@@ -46,32 +59,18 @@ skipneqz r1
 jmp showUsage
 
 mov r1 r0
-mov r0 destArg
+mov r0 buf
 call getpath
 
-; Open source file
-mov r0 sourceArg
-mov r1 FdModeRO
-call openpath
-cmp r1 r0 r0
-skipneqz r1
-jmp couldNotOpenSource
-mov r1 sourceFd
-store8 r1 r0
-
 ; Create/resize dest path
-mov r0 SyscallIdGetFileLen32
-mov r1 sourceArg
-mov r2 fileOffset
-syscall
 mov r2 r0
 mov r0 SyscallIdResizeFile32
-mov r1 destArg
+mov r1 buf
 mov r2 fileOffset
 syscall
 
 ; Open dest
-mov r0 destArg
+mov r0 buf
 mov r1 FdModeWO
 call openpath
 cmp r1 r0 r0
@@ -90,7 +89,7 @@ mov r0 SyscallIdRead32
 mov r1 sourceFd
 load8 r1 r1
 mov r2 fileOffset
-mov r3 copyBuf
+mov r3 buf
 mov r4 1
 syscall
 cmp r0 r0 r0
@@ -101,7 +100,7 @@ mov r0 SyscallIdWrite32
 mov r1 destFd
 load8 r1 r1
 mov r2 fileOffset
-mov r3 copyBuf
+mov r3 buf
 mov r4 1
 syscall
 cmp r0 r0 r0
